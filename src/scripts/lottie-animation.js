55b7c6d8885b07ca49e4370258bd0d9f64ead094
Matheus Amon dos Santos Ferreira <amonmatheus757@gmail.com>
2025-12-03 16:54:29 -0300

    feat: add Ask section

--- src/scripts/lottie-animation.js
+++
@@ -1,8 +0,0 @@
-import { DotLottie } from "@lottiefiles/dotlottie-web";
-
-const dotLottie = new DotLottie({
-  autoplay: true,
-  loop: true,
-  canvas: document.querySelector("#dotlottie-canvas"),
-  src: "../vendor/lottie/free-animation.json",
-});
