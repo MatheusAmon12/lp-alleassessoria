d0794ecae6ebe1fd1e3aac879928baec2760be52
Matheus Amon dos Santos Ferreira <amonmatheus757@gmail.com>
2025-12-04 16:23:21 -0300

    fix: ensure sidebar be closed on nav item click

--- src/scripts/hamburger-menu.js
+++ src/scripts/hamburger-menu.js
@@ -2,6 +2,13 @@ const toggleMenu = () => {
   const menuHamburger = document.getElementById("hamburguer-menu");
   const closeButton = document.getElementById("close-button");
   const sidebar = document.getElementById("sidebar");
+  const navItems = document.querySelectorAll(".nav-item");
+
+  navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      sidebar.style.right = "-100%";
+    });
+  });
 
   menuHamburger.addEventListener("click", () => {
     sidebar.style.right = "0";
